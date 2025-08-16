3a9ed2c034321360d2744c9dc65ba5e45e5c8c1b
mmmm1380                                      <mahdiehhabibi90@gmail.com>
2025-08-16 13:46:07 +0330

    Complete the HandleInput function
    Complete the HandleInput function for mobile touch input and move the ball by touching. Simulate dragging for the mouse for testing.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -24,16 +24,66 @@ public class BallController : MonoBehaviour
     public AudioSource ballKick;
     public AudioSource loselifesound;
     public AudioSource itemsound;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody2D>();
+        if(rb==null){
+            Debug.LogError("Rigidbody2D not found on BallController!");
+        }
+        startPosition=transform.position;
+        defaultMaxSpeed=maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+       // HandleInput();
+        if(!isSpeedBoosted && rb.linearVelocity.magnitude>maxSpeed){
+            rb.linearVelocity=rb.linearVelocity.normalized*maxSpeed;
+        }
         
+    }
+    void HandleInput(){
+        if(Input.touchCount>0){
+            Touch touch=Input.GetTouch(0);
+            Vector2 touchPos=Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(touch.phase==TouchPhase.Began){
+                RaycastHit2D hit=Physics2D.Raycast(touchPos, Vector2.zero);
+                if(hit.collider != null && hit.collider.gameObject == gameObject){
+                    isDragging=true;
+                    dragStartPos=touchPos;
+                   // KickBall();
+                }
+            }
+            else if(touch.phase==TouchPhase.Moved && isDragging){
+                Vector2 delta=touchPos-dragStartPos;
+                rb.AddForce(new Vector2(delta.x * dragSpeed, 0), ForceMode2D.Force);
+                dragStartPos=touchPos;
+            }
+            else if(touch.phase == TouchPhase.Ended){
+                isDragging=false;
+            }
+        }
+        else if(Input.GetMouseButton(0)){
+            Vector2 mousePos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit =Physics2D.Raycast(mousePos, Vector2.zero);
+            if(hit.collider != null && hit.collider.gameObject == gameObject){
+               // KickBall();
+               isDragging=true;
+               dragStartPos=mousePos;
+            }
+        }
+        else if(Input.GetMouseButton(0) && isDragging){
+            Vector2 mousePos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 delta=mousePos-dragStartPos;
+            rb.AddForce(new Vector2(delta.x * dragSpeed, 0), ForceMode2D.Force);
+            dragStartPos=mousePos;
+        }
+        else if(Input.GetMouseButtonUp(0)){
+            isDragging=false;
+        }
     }
 }
