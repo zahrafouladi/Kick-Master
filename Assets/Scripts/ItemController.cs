44c9fe27d6938d0952393cf2c782b45430ae0a83
ghazalfatalian <ghazalfatalian@gmail.com>
2025-08-17 14:58:55 +0330

    Adding the necessary variables
    Adding the necessary variables for ItemController so that this script is assigned separately to the items.

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
+    public GameObject potionObject;
+    public Vector2 spawnAreaMin;
+    public Vector2 spawnAreaMax;
+    public float minInitialDelay=10f;
+    public float maxInitialDelay=7f;
+    public float minSpawnInterval=20f;
+    public float maxSpawnInterval=40f;
+
     // Start is called before the first frame update
     void Start()
     {
+        if(potionObject == null){
+            return;
+        }
+
+        potionObject.SetActive(false);
         
     }
 
